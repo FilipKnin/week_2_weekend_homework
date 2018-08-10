4f5bafe813add0dd0d2e243faa85d7d1b34e0a21
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-10 15:46:47 +0100

    drink_has_name created

--- drink.rb
+++ drink.rb
@@ -1,2 +1,9 @@
-class Drink 
+class Drink
+
+  attr_reader(:name, :price)
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
--- specs/specs_drink.rb
+++ specs/specs_drink.rb
@@ -4,4 +4,12 @@ require_relative('../drink.rb')
 
 class DrinkTest < MiniTest::Test
 
+  def setup
+    @drink1 = Drink.new("Mojito", 10)
+  end
+
+  def test_drink_has_name
+    assert_equal("Mojito", @drink1.name)
+  end
+
 end
