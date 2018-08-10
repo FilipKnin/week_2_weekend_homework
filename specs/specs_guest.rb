2306f34a65e7e9622806e90975cbbed1858bb9fd
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-10 15:41:52 +0100

    Class of drink created

--- specs/specs_guest.rb
+++ specs/specs_guest.rb
@@ -18,7 +18,7 @@ def test_guest_has_money
   assert_equal(250, @guest1.wallet)
 end
 
-def test_guest_has_total_spending__initial_value
+def test_guest_has_total_spending__initial_value_0
   assert_equal(0, @guest1.total_spending)
 end
 
