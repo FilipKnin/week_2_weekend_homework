fc832f05caf7697f5d273dc65593761661fc5931
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-10 17:17:01 +0100

    ccc_has_fee created

--- ccc.rb
+++ ccc.rb
@@ -1,2 +1,11 @@
 class Ccc
+
+  attr_reader(:fee, :rooms, :income, :total_income)
+
+  def initialize(fee, rooms)
+    @fee = fee
+    @rooms = rooms
+    @income = 0
+    @total_income = 0 #is that ok? @total_income = @income
+  end
 end
--- specs/specs_ccc.rb
+++ specs/specs_ccc.rb
@@ -1,7 +1,21 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../ccc.rb')
+require_relative('../room.rb')
+require_relative('../bar.rb')
+require_relative('../drink.rb')
+require_relative('../song.rb')
+require_relative('../guest.rb')
 
 class CccTest < MiniTest::Test
 
+  def setup
+    @room1 = Room.new("Rock'n Roll room", 3, @bar )
+    @Ccc = Ccc.new(15, @room1)
+  end
+
+
+  def test_ccc_has_fee
+    assert_equal(15, @Ccc.fee)
+  end
 end
