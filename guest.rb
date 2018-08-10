ccfbc3fbc9309fb43ddb4432c716d9ca34442d16
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-10 15:20:38 +0100

    guest_has_name created

--- guest.rb
+++ guest.rb
@@ -1,2 +1,10 @@
 class Guest
+
+attr_reader(:name,:wallet, :total_spending )
+
+  def initialize(name, wallet)
+    @name = name
+    @wallet = wallet
+    @total_spending = 0
+  end
 end
--- specs/specs_guest.rb
+++ specs/specs_guest.rb
@@ -4,4 +4,13 @@ require_relative('../guest.rb')
 
 class GuestTest < MiniTest::Test
 
+
+def setup
+  @guest1 = Guest.new("Filip", 250)
+end
+
+
+def test_guest_has_name
+  assert_equal("Filip", @guest1.name)
+end
 end
