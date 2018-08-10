91cf6d604a937ce228b87da4f444c0bed182996e
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-10 16:16:50 +0100

    room_has_name created

--- room.rb
+++ room.rb
@@ -1,2 +1,12 @@
 class Room
+
+  attr_reader(:name, :capacity, :bar, :guests, :songs)
+
+  def initialize(name, capacity, bar)
+    @name = name
+    @capacity = capacity
+    @bar = bar
+    @guests = []
+    @songs = []
+  end
 end
--- specs/specs_room.rb
+++ specs/specs_room.rb
@@ -1,7 +1,21 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../room.rb')
+require_relative('../bar.rb')
+require_relative('../drink.rb')
 
 class RoomTest < MiniTest::Test
+
+  def setup
+    @drink1 = Drink.new("Mojito", 10)
+    @drink2 = Drink.new("Gin", 15)
+    @drink3 = Drink.new("Whiskey", 20)
+    @bar = Bar.new([@drink1, @drink2, @drink3])
+    @room1 = Room.new("Rock'n Roll room", 3, @bar )
+  end
+
+  def test_room_has_name
+    assert_equal("Rock'n Roll room", @room1.name)
+  end
 
 end
